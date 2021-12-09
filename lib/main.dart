cbf8b62d235175080f354932a62a22abb18ca856
Liubov Kostomarova <liubov.kostomarova@gmail.com>
2021-12-09 16:13:29 +0100

    Session Page with navigator

--- lib/main.dart
+++ lib/main.dart
@@ -143,14 +143,14 @@ class SessionTypesListScreen extends StatelessWidget {
         ),
         body: Column(children: [
           Container(
-              padding: const EdgeInsets.all(5),
+              padding: const EdgeInsets.only(left: 20),
               child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text('Session 1',
                       style: TextStyle(
                           fontSize: 35,
                           fontFamily: 'Alegreya',
-                          color: Colors.red)))),
+                          color: Color(0xFFFF0000))))),
           Expanded(
               child: GridView.count(
                   primary: false,
@@ -169,40 +169,55 @@ class SessionTypesListScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(20),
                             image: DecorationImage(
                                 image: ExactAssetImage(session.picture1),
-                                fit: BoxFit.contain)),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image.asset(session.icon),
-                              Text(session.name,
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontFamily: 'Alegreya',
-                                      color: Colors.red))
-                            ])),
+                                fit: BoxFit.cover)),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    image: ExactAssetImage(session.picture2),
+                                    fit: BoxFit.cover)),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: Image.asset(session.icon)),
+                                  Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: Text(session.name,
+                                          style: const TextStyle(
+                                              fontSize: 18,
+                                              fontFamily: 'Alegreya',
+                                              color: Color(0xFFFF0000))))
+                                ]))),
                     onTap: () => onTapped(session),
                   )
               ])),
           BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
-            backgroundColor: Color(0xFF253334),
+            backgroundColor: const Color(0xFF253334),
             elevation: 0,
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                   icon: ImageIcon(
-                      AssetImage('assets/images/Session1/HomeIcon.png')),
+                      AssetImage('assets/images/Session1/HomeIcon.png'),
+                      color: Color(0xFFFF0000)),
                   label: ''),
               BottomNavigationBarItem(
                   icon: ImageIcon(
-                      AssetImage('assets/images/Session1/ChatIcon.png')),
+                      AssetImage('assets/images/Session1/ChatIcon.png'),
+                      color: Color(0xFFC4C4C4)),
                   label: ''),
               BottomNavigationBarItem(
                   icon: ImageIcon(
-                      AssetImage('assets/images/Session1/ConnectionIcon.png')),
+                      AssetImage('assets/images/Session1/DisplaysIcon.png'),
+                      color: Color(0xFFC4C4C4)),
                   label: ''),
               BottomNavigationBarItem(
                   icon: ImageIcon(
-                      AssetImage('assets/images/Session1/Profile_small.png')),
+                      AssetImage('assets/images/Session1/Profile_small.png'),
+                      color: Color(0xFFC4C4C4)),
                   label: ''),
             ],
           )
@@ -227,15 +242,17 @@ class SessionTypesDetailsScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
+          toolbarHeight: 88,
           leading: Image.asset('assets/images/Session1/ConnectionIcon.png'),
-          title:
-              Container(child: Image.asset('assets/images/Session1/Logo.png')),
+          title: IconButton(
+              icon: Image.asset('assets/images/Session1/HomePageLogo.png'),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
           centerTitle: true,
           actions: <Widget>[
             InkWell(
-                onTap: () {
-                  print("It's your profile");
-                },
+                onTap: () {},
                 child: ClipRRect(
                     borderRadius: BorderRadius.circular(30),
                     child: Image.asset(
@@ -244,177 +261,142 @@ class SessionTypesDetailsScreen extends StatelessWidget {
         ),
         body: Column(children: [
           Container(
-              padding: const EdgeInsets.fromLTRB(20, 35, 0, 20),
+              padding: const EdgeInsets.only(left: 20),
               child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(session.title,
                       style: TextStyle(
                           fontSize: 35,
                           fontFamily: 'Alegreya',
-                          color: Colors.red)))),
+                          color: Color(0xFFFF0000))))),
           Expanded(
               child: GridView.count(
                   primary: false,
-                  padding: const EdgeInsets.all(20),
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 20,
+                  padding: const EdgeInsets.all(5),
+                  //crossAxisSpacing: 20,
+                  //mainAxisSpacing: 20,
                   crossAxisCount: 2,
+                  //childAspectRatio: (1 / 1),
+
                   children: <Widget>[
                 if (session != null) ...[
-                  Container(
-                      height: 115,
-                      width: 153,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                              image: ExactAssetImage(
-                                  'assets/images/Session1/RectangleBody1.png'),
-                              fit: BoxFit.contain)),
-                      child: ListTile(
-                        title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image.asset(
-                                  'assets/images/Session1/BodyIcon.png'),
-                              Text(session.name,
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontFamily: 'Alegreya',
-                                      color: Colors.red))
-                            ]),
-                      ))
+                  Column(children: [
+                    Container(
+                        height: 115,
+                        width: 153,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: ExactAssetImage(
+                                    'assets/images/Session1/RectangleBody1.png'),
+                                fit: BoxFit.contain))),
+                    ListTile(
+                      title: Container(
+                          height: 115,
+                          width: 153,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: ExactAssetImage(
+                                      'assets/images/Session1/BodyIcon.png'),
+                                  fit: BoxFit.cover)),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                      image: ExactAssetImage(
+                                          'assets/images/Session1/BodyIcon2.png'),
+                                      fit: BoxFit.cover)),
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                        padding: EdgeInsets.all(5),
+                                        child: Image.asset(session.icon)),
+                                    Padding(
+                                        padding: EdgeInsets.all(5),
+                                        child: Text('Theory',
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                fontFamily: 'Alegreya',
+                                                color: Color(0xFFFF0000))))
+                                  ]))),
+                      onTap: () {},
+                    ),
+                    Container(
+                        height: 115,
+                        width: 153,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: ExactAssetImage(
+                                    'assets/images/Session1/RectangleBody1.png'),
+                                fit: BoxFit.contain))),
+                    ListTile(
+                      title: Container(
+                          height: 115,
+                          width: 153,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: ExactAssetImage(
+                                      'assets/images/Session1/RectanglePsychology.png'),
+                                  fit: BoxFit.cover)),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                      image: ExactAssetImage(
+                                          'assets/images/Session1/RectanglePsychology2.png'),
+                                      fit: BoxFit.cover)),
+                              child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                        padding: EdgeInsets.all(5),
+                                        child: Image.asset(session.icon)),
+                                    Padding(
+                                        padding: EdgeInsets.all(5),
+                                        child: Text('Exercise',
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                fontFamily: 'Alegreya',
+                                                color: Color(0xFFFF0000))))
+                                  ]))),
+                      onTap: () {},
+                    )
+                  ])
                 ]
-              ]))
-          /*Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (session != null) ...[
-              Text(session.name, style: const TextStyle(color: Colors.red)),
-              Image.asset(session.picture1),
+              ])),
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: const Color(0xFF253334),
+            elevation: 0,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                      AssetImage('assets/images/Session1/HomeIcon.png'),
+                      color: Color(0xFFFF0000)),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                      AssetImage('assets/images/Session1/ChatIcon.png'),
+                      color: Color(0xFFC4C4C4)),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                      AssetImage('assets/images/Session1/DisplaysIcon.png'),
+                      color: Color(0xFFC4C4C4)),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                      AssetImage('assets/images/Session1/Profile_small.png'),
+                      color: Color(0xFFC4C4C4)),
+                  label: ''),
             ],
-          ],
-        ),
-      ),*/
+          )
         ]));
   }
 }
-
-
-/*import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
