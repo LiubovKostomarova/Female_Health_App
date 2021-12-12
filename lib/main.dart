d2111bda60a8829074e39931194090bf876694dd
MirraNavy <91861515+MirraNavy@users.noreply.github.com>
2021-12-12 21:04:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,215 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+enum AgeList {first, second, third, forth}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+class _HomeState extends State<Home> {
+
+  final _formKey = GlobalKey<FormState>();
+  AgeList _age = AgeList.first;
+  bool _agreement = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return SafeArea(
+        child: Scaffold(
+            body: Container (
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage ("assets/images/Back.png"), fit: BoxFit.cover
+              )
+              ),
+              child: Column (
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 411,
+                    height: 100,
+                    alignment: Alignment.topCenter,
+                    child: Image(
+                      image: AssetImage("assets/images/Logo.png"),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: 311,
+                    height: 78,
+                  child:
+                  Text("Let's make it personal", style: TextStyle(fontSize: 30, color: Color(0xffFF0000), fontFamily: "Alegreya"), ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 311,
+                    height: 78,
+                    child:
+                    Text("Answer several questions that will help with right track and results monitoring", style: TextStyle(fontSize: 22, color: Colors.grey,fontFamily: "AlegreyaSans") ,),
+                  ),
+                  SizedBox(height: 50),
+                  Expanded(
+                    child:
+                  Container(
+                      child: Container(
+                        width: 311,
+                        height: 206,
+                        color: Color(0xffc4c4c4),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment(-0.7,0),
+                              child:
+                            Text("Your age:", style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: "AlegreyaSans"),)
+                            ),
+                            SizedBox(height: 20),
+                            RadioListTile(
+                              title: const Text('18-22', style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: "AlegreyaSans")),
+                              value: AgeList.first,
+                              groupValue: _age,
+                              onChanged: (AgeList? value) {
+                                setState(() { _age = value!;});
+                              },
+                            ),
+                            RadioListTile(
+                              title: const Text('22-26', style: TextStyle(fontSize: 22, color: Colors.black,fontFamily: "AlegreyaSans")),
+                              value: AgeList.second,
+                              groupValue: _age,
+                              onChanged: (AgeList? value) {
+                                setState(() { _age = value!;});
+                              },
+                            ),
+                            RadioListTile(
+                              title: const Text('27-31', style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: "AlegreyaSans")),
+                              value: AgeList.third,
+                              groupValue: _age,
+                              onChanged: (AgeList? value) {
+                                setState(() { _age = value!;});
+                              },
+                            ),
+                            RadioListTile(
+                              title: const Text('31-35', style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: "AlegreyaSans")),
+                              value: AgeList.forth,
+                              groupValue: _age,
+                              onChanged: (AgeList? value) {setState(() { _age = value!;});},
+                            ),
+                          ],
+                        ),
+                      )
+                  ),
+                  ),
+                  SizedBox(height: 30),
+
+                  ElevatedButton (
+                    child: Text("Finish", style: TextStyle(fontSize: 25, color: Color(0xffFF0000), fontFamily: "AlegreyaSans"),),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff7c9a92),
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 90),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)
+                      )
+                    ),
+                      onPressed:() {
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return NextScreen();
+                      }),
+                      );
+                      },
+    ),
+                  SizedBox(height: 30),
+              ]
+    )
+                  )
+              ),
+            );
   }
 }
+
+class NextScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container (
+          alignment: Alignment.topCenter,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage ("assets/images/Back.png"), fit: BoxFit.cover
+                )
+            ),
+            child: Column (
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 411,
+                  height: 130,
+                  alignment: Alignment.topCenter,
+                  child: Image(
+                    image: AssetImage("assets/images/Logo.png"),
+                  ),
+                ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              alignment: Alignment.topLeft,
+              width: 311,
+              height: 100,
+              child:
+              Text("Let's link the device", style: TextStyle(fontSize: 30, color: Color(0xffFF0000), fontFamily: "Alegreya"),),
+            ),
+
+                Container(
+              child: Container(
+                width: 311,
+                height: 430,
+                color: Color(0xffc4c4c4),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 311,
+                  height: 78,
+                child: Text("[Here will be the instructions on how to connect the device and the qualiti of the connection monitoring]", style: TextStyle(fontSize: 22, color: Colors.black, fontFamily: "AlegreyaSans")),
+                  padding: EdgeInsets.all(15),
+                )
+              )
+            ),
+                SizedBox(height: 30),
+
+                ElevatedButton (onPressed:() {Navigator.pop(context);},
+                    child: Text("Finish", style: TextStyle(fontSize: 25, color: Color(0xffFF0000), fontFamily: "AlegreyaSans"),),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xff7c9a92),
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 90),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)
+                        )
+                    )
+                ),
+                SizedBox(height: 30),
+                ],
+              ),
+            )
+        );
+  }
+}
+
