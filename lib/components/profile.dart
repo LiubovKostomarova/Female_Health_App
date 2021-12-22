55ac7fa6d59ae7c7bca4d4311a36e8d1806c1ead
moroz23moroz <moroz2387moroz@yandex.ru>
2021-12-22 03:35:36 +0300

    add new

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:reverie/constants.dart';
 
 class MyProfileScreen extends StatelessWidget {
-  const MyProfileScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
