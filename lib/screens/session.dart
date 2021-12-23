374b8f9cefde5d050987f0a5adde4456c822efe9
Liubov Kostomarova <liubov.kostomarova@gmail.com>
2021-12-23 17:23:06 +0100

    changes

--- lib/screens/session.dart
+++ lib/screens/session.dart
@@ -2,23 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:reverie/components/base_appbar.dart';
 import 'package:reverie/components/bottom_menu.dart';
-import 'package:reverie/screens/details_page.dart';
-
-/*void main() {
-  runApp(SessionTypesApp());
-}*/
+//import 'package:reverie/screens/details_page.dart';
 
 class SessionType {
-  final List card;
-  final String title;
-  final String icon;
-  final String icon2;
-  final String name;
-  final String picture1;
-  final String picture2;
+  List<SessionType> cards;
+  String title;
+  String icon;
+  bool isPremium;
+  String picture1;
+  String picture2;
 
-  SessionType(this.card, this.title, this.icon, this.icon2, this.name,
-      this.picture1, this.picture2);
+  SessionType(this.title, this.icon, this.picture1, this.picture2, this.cards,
+      {this.isPremium = false});
 }
 
 class SessionTypesApp extends StatefulWidget {
@@ -30,76 +25,66 @@ class _SessionTypesAppState extends State<SessionTypesApp> {
   SessionType _selectedSessionType;
 
   List<SessionType> sessions = [
-    SessionType([
-      'Body (Session 1)',
-      'assets/images/Session1/BodyIcon.png',
-      '',
-      'Body',
-      'assets/images/Session1/RectangleBody1.png',
-      'assets/images/Session1/RectangleBody2.png'
-    ],
-        'Body (Session 1)',
-        'assets/images/Session1/BodyIcon.png',
-        '',
+    SessionType(
         'Body',
+        'assets/images/Session1/BodyIcon.png',
         'assets/images/Session1/RectangleBody1.png',
-        'assets/images/Session1/RectangleBody2.png'),
-    SessionType([
-      'Psychology (Session 1)',
-      'assets/images/Session1/PsychologyIcon.png',
-      '',
-      'Psychology',
-      'assets/images/Session1/RectanglePsychology.png',
-      'assets/images/Session1/RectanglePsychology2.png'
-    ],
-        'Psychology (Session 1)',
-        'assets/images/Session1/PsychologyIcon.png',
-        '',
+        'assets/images/Session1/RectangleBody2.png', [
+      SessionType(
+          'Theory',
+          'assets/images/Session1/BodyIcon.png',
+          'assets/images/Session1/RectangleBody1.png',
+          'assets/images/Session1/RectangleBody2.png', []),
+      SessionType(
+          'Excersize',
+          'assets/images/Session1/PsychologyIcon.png',
+          'assets/images/Session1/RectanglePsychology.png',
+          'assets/images/Session1/RectanglePsychology2.png', [])
+    ]),
+    SessionType(
         'Psychology',
+        'assets/images/Session1/PsychologyIcon.png',
         'assets/images/Session1/RectanglePsychology.png',
-        'assets/images/Session1/RectanglePsychology2.png'),
-    SessionType([
-      'Partner (Session 1)',
-      'assets/images/Session1/PartnerIcon.png',
-      '',
-      'Partner',
-      'assets/images/Session1/RectanglePartner1.png',
-      'assets/images/Session1/RectanglePartner2.png'
-    ],
-        'Partner (Session 1)',
-        'assets/images/Session1/PartnerIcon.png',
-        '',
+        'assets/images/Session1/RectanglePsychology2.png', [
+      SessionType(
+          'Theory',
+          'assets/images/Session1/BodyIcon.png',
+          'assets/images/Session1/RectangleBody1.png',
+          'assets/images/Session1/RectangleBody2.png', []),
+      SessionType(
+          'Excersize',
+          'assets/images/Session1/PsychologyIcon.png',
+          'assets/images/Session1/RectanglePsychology.png',
+          'assets/images/Session1/RectanglePsychology2.png', [])
+    ]),
+    SessionType(
         'Partner',
+        'assets/images/Session1/PartnerIcon.png',
         'assets/images/Session1/RectanglePartner1.png',
-        'assets/images/Session1/RectanglePartner2.png'),
-    SessionType([
-      'Practice with device (Session 1)',
-      'assets/images/Session1/TickIcon.png',
-      '',
-      'Practice with device',
-      'assets/images/Session1/RectanglePractice.png',
-      'assets/images/Session1/RectanglePractice2.png'
-    ],
-        'Practice with device (Session 1)',
-        'assets/images/Session1/TickIcon.png',
-        '',
+        'assets/images/Session1/RectanglePartner2.png', [
+      SessionType(
+          'Theory',
+          'assets/images/Session1/BodyIcon.png',
+          'assets/images/Session1/RectangleBody1.png',
+          'assets/images/Session1/RectangleBody2.png', []),
+      SessionType(
+          'Excersize',
+          'assets/images/Session1/PsychologyIcon.png',
+          'assets/images/Session1/RectanglePsychology.png',
+          'assets/images/Session1/RectanglePsychology2.png', [])
+    ]),
+    SessionType(
         'Practice with device',
+        'assets/images/Session1/TickIcon.png',
         'assets/images/Session1/RectanglePractice.png',
-        'assets/images/Session1/RectanglePractice2.png'),
-    SessionType([
-      'Diary (Session 1)',
-      'assets/images/Session1/DiaryIcon1.png',
-      'assets/images/Session1/DiaryIcon2.png',
-      'Diary',
-      'assets/images/Session1/RectangleDiary.png',
-      'assets/images/Session1/RectangleDiary2.png'
-    ],
-        'Diary (Session 1)',
-        'assets/images/Session1/DiaryIcon1.png',
-        'assets/images/Session1/DiaryIcon2.png',
+        'assets/images/Session1/RectanglePractice2.png', []),
+    SessionType(
         'Diary',
+        'assets/images/Session1/DiaryIcon1.png',
         'assets/images/Session1/RectangleDiary.png',
-        'assets/images/Session1/RectangleDiary2.png'),
+        'assets/images/Session1/RectangleDiary2.png',
+        [],
+        isPremium: true)
   ];
 
   @override
@@ -152,7 +137,7 @@ class SessionTypesDetailsPage extends Page {
     return MaterialPageRoute(
       settings: this,
       builder: (BuildContext context) {
-        return SessionTypesDetailsScreen(session: session);
+        return SessionTypesListScreen(sessions: session.cards);
       },
     );
   }
@@ -161,6 +146,12 @@ class SessionTypesDetailsPage extends Page {
 class SessionTypesListScreen extends StatelessWidget {
   final List<SessionType> sessions;
   final ValueChanged<SessionType> onTapped;
+  String title = 'Session 1';
+  final List<String> _headers = <String>[
+    "Body (Session 1)",
+    "Psychology (Session 1)",
+    "Partner (Session 1)",
+  ];
 
   SessionTypesListScreen({
     @required this.sessions,
@@ -177,7 +168,7 @@ class SessionTypesListScreen extends StatelessWidget {
               padding: const EdgeInsets.only(left: 20, top: 20),
               child: Align(
                   alignment: Alignment.centerLeft,
-                  child: Text('Session 1',
+                  child: Text(title,
                       style: TextStyle(
                           fontSize: 35,
                           fontFamily: 'Alegreya',
@@ -211,13 +202,16 @@ class SessionTypesListScreen extends StatelessWidget {
                                 children: [
                                   Padding(
                                       padding: EdgeInsets.all(5),
-                                      child: Image.asset(session.icon2)),
-                                  Padding(
-                                      padding: EdgeInsets.all(5),
                                       child: Image.asset(session.icon)),
                                   Padding(
                                       padding: EdgeInsets.all(5),
-                                      child: Text(session.name,
+                                      child: session.isPremium
+                                          ? Image.asset(
+                                              'assets/images/Session1/DiaryIcon2.png')
+                                          : null),
+                                  Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: Text(session.title,
                                           style: const TextStyle(
                                               fontSize: 18,
                                               fontFamily: 'Alegreya',
